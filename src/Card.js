054dae08f211a5468474a7b72de716642d07623b
LukasP <piecuchlukaszuk@gmail.com>
2020-03-09 18:02:42 +0000

    spatanie

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+	return (
+		<div>
+			<img alt='Robot' src='https://robohash.org/test?size=200x200' />
+			<div>
+				<h2>John Doe</h2>
+				<p>John.doe@gmail.com</p>
+			</div>
+		</div>
+	);
+}
+
+export default Card;
